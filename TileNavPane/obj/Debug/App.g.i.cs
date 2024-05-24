03ca731014e86fb03e89619f2a3a623201fb10fd
Joel Orlando Malibran Escamilla <joelorlandom@gmail.com>
2024-05-24 12:52:17 -0600

    Se preparo el proyecto para empezar a trabajarlo con el patron MVVM

--- TileNavPane/obj/Debug/App.g.i.cs
+++ TileNavPane/obj/Debug/App.g.i.cs
@@ -42,20 +42,6 @@ namespace TileNavPane {
     public partial class App : System.Windows.Application {
         
         /// <summary>
-        /// InitializeComponent
-        /// </summary>
-        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
-        public void InitializeComponent() {
-            
-            #line 5 "..\..\App.xaml"
-            this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
-            
-            #line default
-            #line hidden
-        }
-        
-        /// <summary>
         /// Application Entry Point.
         /// </summary>
         [System.STAThreadAttribute()]
@@ -63,7 +49,6 @@ namespace TileNavPane {
         [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
         public static void Main() {
             TileNavPane.App app = new TileNavPane.App();
-            app.InitializeComponent();
             app.Run();
         }
     }
